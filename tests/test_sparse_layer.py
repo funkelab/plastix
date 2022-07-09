cb642fd78f315d614b37c004b499003475e97f26
Yash Mehta <yashsmehta95@gmail.com>
2022-07-09 12:59:32 -0400

    Add additional tests for sparse layer

--- tests/test_sparse_layer.py
+++ tests/test_sparse_layer.py
@@ -4,22 +4,20 @@ import unittest
 
 
 class TestSparseLayer(unittest.TestCase):
-
     def test_and(self):
 
         n = 2
         m = 1
         # dense layer as sparse layer
-        edges = [
-            (0, 0),
-            (1, 0)
-        ]
+        edges = [(0, 0), (1, 0)]
 
         layer = px.layers.SparseLayer(
-            n, m,
+            n,
+            m,
             edges,
             px.kernels.edges.FixedWeight(),
-            px.kernels.nodes.SumNonlinear())
+            px.kernels.nodes.SumNonlinear(),
+        )
 
         state = layer.init_state()
         parameters = layer.init_parameters()
@@ -54,16 +52,15 @@ class TestSparseLayer(unittest.TestCase):
         n = 200
         m = 100
         # dense layer as sparse layer
-        edges = [
-            (42, 99),
-            (23, 99)
-        ]
+        edges = [(42, 99), (23, 99)]
 
         layer = px.layers.SparseLayer(
-            n, m,
+            n,
+            m,
             edges,
             px.kernels.edges.FixedWeight(),
-            px.kernels.nodes.SumNonlinear())
+            px.kernels.nodes.SumNonlinear(),
+        )
 
         state = layer.init_state()
         parameters = layer.init_parameters()
@@ -97,3 +94,44 @@ class TestSparseLayer(unittest.TestCase):
         state.input_nodes.rate = jnp.array(inputs)
         state = layer.update_state(state, parameters)
         assert state.output_nodes.rate[99] > 0.5
+
+    def test_sparse_dense(self):
+        dense_layer = px.layers.DenseLayer(
+            5,
+            3,
+            px.kernels.edges.FixedWeight(),
+            px.kernels.nodes.SumNonlinear(),
+        )
+        edges = [(i, j) for i in range(5) for j in range(3)]
+        sparse_layer = px.layers.SparseLayer(
+            5,
+            3,
+            edges,
+            px.kernels.edges.FixedWeight(),
+            px.kernels.nodes.SumNonlinear(),
+        )
+        dl_state = dense_layer.init_state()
+        dl_parameters = dense_layer.init_parameters()
+        sl_state = sparse_layer.init_state()
+        sl_parameters = sparse_layer.init_parameters()
+
+        dl_parameters.edges.weight *= 0.5
+        sl_parameters.edges.weight *= 0.5
+
+        # 0, 0, 0, 0, 0 -> 0, 0, 0
+        layer_input = jnp.array([[0.0] for _ in range(5)])
+        dl_state.input_nodes.rate = layer_input
+        sl_state.input_nodes.rate = layer_input
+
+        dl_state = dense_layer.update_state(dl_state, dl_parameters)
+        sl_state = sparse_layer.update_state(sl_state, sl_parameters)
+        assert (dl_state.output_nodes.rate == sl_state.output_nodes.rate).all()
+
+        # 1, 1, 0, 0, 0 -> 0.96, 0.96, 0.96
+        layer_input = jnp.array([[0.0] for _ in range(5)])
+        dl_state.input_nodes.rate = layer_input
+        sl_state.input_nodes.rate = layer_input
+
+        dl_state = dense_layer.update_state(dl_state, dl_parameters)
+        sl_state = sparse_layer.update_state(sl_state, sl_parameters)
+        assert (dl_state.output_nodes.rate == sl_state.output_nodes.rate).all()
